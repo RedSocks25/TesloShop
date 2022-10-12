18679d578e7f392b8a855e34a2dac727cc01ce9c
Nicolás Selman <nicolas.selman.25@gmail.com>
2022-10-12 10:35:21 -0600

    Product List and Product Card as components

---
+++ components/products/ProductList.tsx
@@ -0,0 +1,21 @@
+import React, { FC, PropsWithChildren } from 'react';
+
+import { Grid } from '@mui/material';
+
+import { IProduct } from '../../interfaces';
+import { ProductCard } from './ProductCard';
+
+
+interface Props extends PropsWithChildren {
+  products: IProduct[],
+}
+
+export const ProductList: FC<Props> = ({ products }) => {
+  return (
+    <Grid container spacing={ 4 }>
+      { products.map( product => (
+        <ProductCard product={ product } key={ product.slug } />
+      ))}
+    </Grid>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from 'next';
 
-import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 
 import { ShopLayout } from '../components/layouts/ShopLayout';
 import { initialData } from '../database/products';
+import { ProductList } from '../components/products';
 
 
 const Home: NextPage = () => {
@@ -14,22 +15,7 @@ const Home: NextPage = () => {
       <Typography variant='h1' component='h1'>HomePage</Typography> 
       <Typography variant='h2' sx={{ mb: 1 }}>All the products</Typography> 
 
-      <Grid container spacing={ 4 }>
-        { initialData.products.map(product => (
-          <Grid item xs={ 6 } sm={ 4 } key={ product.slug }>
-            <Card>
-              <CardActionArea>
-                <CardMedia
-                  component='img'
-                  image={ `products/${ product.images[0] }` }
-                  alt={ product.title }
-                />
-              </CardActionArea>
-            </Card>
-          </Grid>
-        ))}
-        
-      </Grid>
+      <ProductList products={ initialData.products as any } />
     
     </ShopLayout>
   );
