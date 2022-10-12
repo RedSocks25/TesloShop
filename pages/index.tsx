2f3770a2fb3248531297491a0cff04c72a829dd7
Nicolás Selman <nicolas.selman.25@gmail.com>
2022-10-12 00:48:43 -0600

    Added temporal HomePage design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import type { NextPage } from 'next';
 
-import { Typography } from '@mui/material';
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
 
 import { ShopLayout } from '../components/layouts/ShopLayout';
+import { initialData } from '../database/products';
 
 
 const Home: NextPage = () => {
@@ -12,6 +13,23 @@ const Home: NextPage = () => {
       {/* component H1 help the SEO to know that this is a title and is important */}
       <Typography variant='h1' component='h1'>HomePage</Typography> 
       <Typography variant='h2' sx={{ mb: 1 }}>All the products</Typography> 
+
+      <Grid container spacing={ 4 }>
+        { initialData.products.map(product => (
+          <Grid item xs={ 6 } sm={ 4 } key={ product.slug }>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component='img'
+                  image={ `products/${ product.images[0] }` }
+                  alt={ product.title }
+                />
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+        
+      </Grid>
     
     </ShopLayout>
   );
