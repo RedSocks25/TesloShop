2ab1f5e5e510ae56ec221e95b271d46beacc0bd1
Nicolás Selman <nicolas.selman.25@gmail.com>
2022-10-12 00:11:41 -0600

    ShopLayout added and metatags

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,17 @@
+import { Typography } from '@mui/material';
 import type { NextPage } from 'next';
+
+import { ShopLayout } from '../components/layouts/ShopLayout';
 
 const Home: NextPage = () => {
   return (
-    <h1>TesloShop</h1>
+    <ShopLayout title={'Teslo Shop - Home'} pageDescription={'Find the best products here'}>
+
+      {/* component H1 help the SEO to know that this is a title and is important */}
+      <Typography variant='h1' component='h1'>HomePage</Typography> 
+      <Typography variant='h2' sx={{ mb: 1 }}>All the products</Typography> 
+    
+    </ShopLayout>
   );
 }
 
