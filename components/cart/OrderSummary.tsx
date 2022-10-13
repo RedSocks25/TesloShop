035e8e37eeceaa5dc4b2a1f2fa60acaeefa98f43
Nicolás Selman <nicolas.selman.25@gmail.com>
2022-10-13 09:24:58 -0600

    Cart Product List and Summary Page

---
+++ components/cart/OrderSummary.tsx
@@ -0,0 +1,38 @@
+import { Divider, Grid, Typography } from '@mui/material';
+import React from 'react';
+
+export const OrderSummary = () => {
+  return (
+    <Grid container>
+
+      <Grid item xs={ 6 }>
+        <Typography>No. of products</Typography>
+      </Grid>
+      <Grid item xs={ 6 } display='flex' justifyContent='end'>
+        <Typography>3</Typography>
+      </Grid>
+
+      <Grid item xs={ 6 }>
+        <Typography>Subtotal</Typography>
+      </Grid>
+      <Grid item xs={ 6 } display='flex' justifyContent='end'>
+        <Typography>{ `$${ 155.36 }`}</Typography>
+      </Grid>
+
+      <Grid item xs={ 6 }>
+        <Typography>Taxes (15%)</Typography>
+      </Grid>
+      <Grid item xs={ 6 } display='flex' justifyContent='end'>
+        <Typography>{ `$${ 23.304 }`}</Typography>
+      </Grid>
+
+      <Grid item xs={ 6 } sx={{ mt: 2 }}>
+        <Typography variant='subtitle1'>Total to pay</Typography>
+      </Grid>
+      <Grid item xs={ 6 } sx={{ mt: 2 }} display='flex' justifyContent='end'>
+        <Typography variant='subtitle1'>{ `$${ 178.664 }`}</Typography>
+      </Grid>
+
+    </Grid>
+  );
+}
--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,8 +1,42 @@
 import React from 'react';
+import { ShopLayout } from '../../components/layouts';
+import { Grid, Typography, Card, CardContent, Divider, Box, Button } from '@mui/material';
+import { CartList } from '../../components/cart';
+import { OrderSummary } from '../../components/cart/OrderSummary';
+
 
 const CartPage = () => {
   return (
-    <div>CartPage</div>
+    <ShopLayout title='Cart - 3' pageDescription='Shopping cart'>
+      <Typography variant='h1' component='h1'>Cart</Typography>
+
+      <Grid container>
+      
+        <Grid item xs={ 12 } sm={ 7 }>
+          <CartList />
+        </Grid>
+
+        <Grid item xs={ 12 } sm={ 5 }>
+          <Card className='summary-card'>
+            <CardContent>
+
+              <Typography variant='h2'>Your order</Typography>
+              <Divider sx={{ my: 1 }} />
+
+              <OrderSummary />
+
+              <Box sx={{ mt: 3 }}>
+                <Button color='secondary' className='circular-btn' fullWidth>
+                  Checkout
+                </Button>
+              </Box>
+
+            </CardContent>
+          </Card>
+        </Grid>
+      
+      </Grid>
+    </ShopLayout>
   );
 }
 
